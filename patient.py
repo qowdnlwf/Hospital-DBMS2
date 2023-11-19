80f39a06f9d114fa902fcca2d80ac8e206a1c6af
glgjssy <glgjssy81@gmail.com>
2023-11-19 15:59:39 +0800

    init

--- patient.py
+++ patient.py
@@ -22,9 +22,9 @@ def verify_patient_id(patient_id):
     return verify
 
 # function to generate unique patient id using current date and time
-def generate_patient_id(reg_date, reg_time):
-    id_1 = ''.join(reg_time.split(':')[::-1])
-    id_2 = ''.join(reg_date.split('-')[::-1])[2:]
+def generate_patient_id():
+    id_1 = datetime.now().strftime('%S%M%H')
+    id_2 = datetime.now().strftime('%Y%m%d')[2:]
     id = f'P-{id_1}-{id_2}'
     return id
 
@@ -37,12 +37,7 @@ def calculate_age(dob):
 # function to show the details of patient(s) given in a list (provided as a parameter)
 def show_patient_details(list_of_patients):
     patient_titles = ['Patient ID', 'Name', 'Age', 'Gender', 'Date of birth (DD-MM-YYYY)',
-                     'Blood group', 'Contact number', 'Alternate contact number',
-                     'Aadhar ID / Voter ID', 'Weight (kg)', 'Height (cm)', 'Address',
-                     'City', 'State', 'PIN code', "Next of kin's name",
-                     "Next of kin's relation to patient",
-                     "Next of kin's contact number", 'Email ID',
-                     'Date of registration (DD-MM-YYYY)', 'Time of registration (hh:mm:ss)']
+                     'Blood group', 'Contact number',  'Weight (kg)', 'Height (cm)', 'Address',]
     if len(list_of_patients) == 0:
         st.warning('No data to show')
     elif len(list_of_patients) == 1:
@@ -64,23 +59,12 @@ class Patient:
         self.id = str()
         self.gender = str()
         self.age = int()
-        self.contact_number_1 = str()
-        self.contact_number_2 = str()
+        self.contact_number = str()
         self.date_of_birth = str()
         self.blood_group = str()
-        self.date_of_registration = str()
-        self.time_of_registration = str()
-        self.email_id = str()
-        self.aadhar_or_voter_id = str()
         self.height = int()
         self.weight = int()
-        self.next_of_kin_name = str()
-        self.next_of_kin_relation_to_patient = str()
-        self.next_of_kin_contact_number = str()
         self.address = str()
-        self.city = str()
-        self.state = str()
-        self.pin_code = str()
 
     # method to add a new patient record to the database
     def add_patient(self):
@@ -94,25 +78,11 @@ class Patient:
         self.date_of_birth = dob.strftime('%d-%m-%Y')       # converts date of birth to the desired string format
         self.age = calculate_age(dob)
         self.blood_group = st.text_input('Blood group')
-        self.contact_number_1 = st.text_input('Contact number')
-        contact_number_2 = st.text_input('Alternate contact number (optional)')
-        self.contact_number_2 = (lambda phone : None if phone == '' else phone)(contact_number_2)
-        self.aadhar_or_voter_id = st.text_input('Aadhar ID / Voter ID')
+        self.contact_number = st.text_input('Contact number')
         self.weight = st.number_input('Weight (in kg)', value = 0, min_value = 0, max_value = 400)
         self.height = st.number_input('Height (in cm)', value = 0, min_value = 0, max_value = 275)
         self.address = st.text_area('Address')
-        self.city = st.text_input('City')
-        self.state = st.text_input('State')
-        self.pin_code = st.text_input('PIN code')
-        self.next_of_kin_name = st.text_input("Next of kin's name")
-        self.next_of_kin_relation_to_patient = st.text_input("Next of kin's relation to patient")
-        self.next_of_kin_contact_number = st.text_input("Next of kin's contact number")
-        email_id = st.text_input('Email ID (optional)')
-        self.email_id = (lambda email : None if email == '' else email)(email_id)
-        self.date_of_registration = datetime.now().strftime('%d-%m-%Y')
-        self.time_of_registration = datetime.now().strftime('%H:%M:%S')
-        self.id = generate_patient_id(self.date_of_registration,
-                    self.time_of_registration)
+        self.id = generate_patient_id()
         save = st.button('Save')
 
         # executing SQLite statements to save the new patient record to the database
@@ -124,11 +94,8 @@ class Patient:
                     INSERT INTO patient_record
                     (
                         id, name, age, gender, date_of_birth, blood_group,
-                        contact_number_1, contact_number_2, aadhar_or_voter_id,
-                        weight, height, address,city, state, pin_code,
-                        next_of_kin_name, next_of_kin_relation_to_patient,
-                        next_of_kin_contact_number, email_id,
-                        date_of_registration, time_of_registration
+                        contact_number, 
+                        weight, height, address, room, history
                     )
                     VALUES (
                         :id, :name, :age, :gender, :dob, :blood_group,
